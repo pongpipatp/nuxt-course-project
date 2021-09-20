d53ff2f7b053f3d5b535d3a3456bffe69a629ac1
pongpipatp <53258097+pongpipatp@users.noreply.github.com>
2021-09-20 11:55:17 +0700

    [Editted]: Touch up

--- Lecture_3/components/Posts/PostList.vue
+++ Lecture_3/components/Posts/PostList.vue
@@ -10,14 +10,14 @@
       id="2"
       :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      title="Hello there - the second time!"
+      title="Hello there - the second time!!!!"
       previewText="This my second post!" />
     <PostPreview
       id="3"
       :is-admin="isAdmin"
       thumbnail="https://static.pexels.com/photos/270348/pexels-photo-270348.jpeg"
-      title="Hi!"
-      previewText="This my third post!" />
+      title="Hi! - the Third time!!!"
+      previewText="This my third post!!!!!!!!" />
   </section>
 </template>
 
